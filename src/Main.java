f435c0366a410ffb2befc6e7615f538fc7d2237c
tatsuya828828 <kkjy011894@gmail.com>
2020-06-30 16:33:06 +0900

    [finish]16-4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-import java.util.List;
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 public class Main {
-
-	public static void printList(List<String> list) {
-		list.add("たかし");
-		list.add("たいぞう");
-		for(String s : list) {
-			System.out.println(s);
+	public static void main(String[] args) {
+		// Setの基本特性
+		// それぞれの要素には、重複が許されない
+		// それぞれの要素には、基本的に順序関係がない
+		Set<String> colors = new HashSet<String>();
+		colors.add("赤");
+		colors.add("青");
+		colors.add("黄");
+		colors.add("赤");
+		colors.add("藍");
+		colors.add("茶");
+		System.out.println("色は"+colors.size()+"種類です");
+		for(String s : colors) {
+			System.out.print(s + "→");
 		}
 
-	}
-	public static void main(String[] args) {
+		System.out.println("");
+
+		Set<String> words = new TreeSet<String>();
+		words.add("dog");
+		words.add("cat");
+		words.add("wolf");
+		words.add("panda");
+		words.add("あ");
+		words.add("い");
+		words.add("さ");
+		words.add("か");
+		for(String s :words) {
+			System.out.print(s + "→");
+		}
 	}
 }
