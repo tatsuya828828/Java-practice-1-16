f628cb3a6e4eb57b77d01844dd94febefa468b91
tatsuya828828 <kkjy011894@gmail.com>
2020-06-30 17:46:24 +0900

    [finish]16-5

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,23 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 	public static void main(String[] args) {
-		// Setの基本特性
-		// それぞれの要素には、重複が許されない
-		// それぞれの要素には、基本的に順序関係がない
-		Set<String> colors = new HashSet<String>();
-		colors.add("赤");
-		colors.add("青");
-		colors.add("黄");
-		colors.add("赤");
-		colors.add("藍");
-		colors.add("茶");
-		System.out.println("色は"+colors.size()+"種類です");
-		for(String s : colors) {
-			System.out.print(s + "→");
-		}
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都府", 255);
+		prefs.put("東京都", 1261);
+		prefs.put("熊本県", 181);
+		int tokyo = prefs.get("東京都");
+		System.out.println("東京都の人口は"+tokyo);
+		// prefs.remove("京都府");
+		prefs.put("熊本県", 182);
+		int kumamoto = prefs.get("熊本県");
+		System.out.println("熊本県の人口は"+kumamoto);
 
-		System.out.println("");
-
-		Set<String> words = new TreeSet<String>();
-		words.add("dog");
-		words.add("cat");
-		words.add("wolf");
-		words.add("panda");
-		words.add("あ");
-		words.add("い");
-		words.add("さ");
-		words.add("か");
-		for(String s :words) {
-			System.out.print(s + "→");
+		System.out.println("人口一覧");
+		for(String key: prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key+"の人口は"+value);
 		}
 	}
 }
